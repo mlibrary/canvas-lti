afc32c45bf9d2590c270c27a578805cbc700af02
el1_1el <eliotwsc@umich.edu>
2022-01-05 13:39:25 -0500

    Revert "roll back lti_tool_provider until PECL OAuth library installed"
    This reverts commit a8678f1dfa94d0793a2d8edfa3327f52a872c7a1.

--- modules/contrib/lti_tool_provider/src/Event/LtiToolProviderLaunchEvent.php
+++ modules/contrib/lti_tool_provider/src/Event/LtiToolProviderLaunchEvent.php
@@ -2,39 +2,59 @@
 
 namespace Drupal\lti_tool_provider\Event;
 
-use Drupal\lti_tool_provider\LtiToolProviderEvent;
+use Drupal\lti_tool_provider\LTIToolProviderContextInterface;
+use Symfony\Component\EventDispatcher\Event;
 
-class LtiToolProviderLaunchEvent extends LtiToolProviderEvent
-{
-    const EVENT_NAME = 'LTI_TOOL_PROVIDER_LAUNCH_EVENT';
+class LtiToolProviderLaunchEvent extends Event {
 
-    /**
-     * @var array
-     */
-    private $context;
+  /**
+   * @var \Drupal\lti_tool_provider\LTIToolProviderContextInterface
+   */
+  private $context;
 
-    /**
-     * LtiToolProviderLaunchEvent constructor.
-     * @param array $context
-     */
-    public function __construct(array $context)
-    {
-        $this->setContext($context);
-    }
+  /**
+   * @var string
+   */
+  private $destination;
 
-    /**
-     * @return array
-     */
-    public function getContext(): array
-    {
-        return $this->context;
-    }
+  /**
+   * LtiToolProviderLaunchEvent constructor.
+   *
+   * @param \Drupal\lti_tool_provider\LTIToolProviderContextInterface $context
+   *
+   * @param string $destination
+   */
+  public function __construct(LTIToolProviderContextInterface $context, string $destination) {
+    $this->setContext($context);
+    $this->setDestination($destination);
+  }
 
-    /**
-     * @param array $context
-     */
-    public function setContext(array $context)
-    {
-        $this->context = $context;
-    }
+  /**
+   * @return \Drupal\lti_tool_provider\LTIToolProviderContextInterface
+   */
+  public function getContext(): LTIToolProviderContextInterface {
+    return $this->context;
+  }
+
+  /**
+   * @param \Drupal\lti_tool_provider\LTIToolProviderContextInterface $context
+   */
+  public function setContext(LTIToolProviderContextInterface $context) {
+    $this->context = $context;
+  }
+
+  /**
+   * @return string
+   */
+  public function getDestination(): string {
+    return $this->destination;
+  }
+
+  /**
+   * @param string $destination
+   */
+  public function setDestination(string $destination): void {
+    $this->destination = $destination;
+  }
+
 }
