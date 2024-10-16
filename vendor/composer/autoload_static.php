05ca4f06688e56d89b9259ea010a3b7bf7601de3
Eliot Scott <eliotwsc@umich.edu>
2024-10-16 14:32:14 -0400

    core 10.2.10 update

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -391,8 +391,8 @@ class ComposerStaticInit243c0f8eb445e797e727f14305cf6395
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
